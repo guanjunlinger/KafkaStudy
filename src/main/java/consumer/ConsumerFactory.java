e5ce3b8f616c24026ce89ac6e873525395bd9ea2
guanjun <guanjun@kingsoft.com>
2020-10-14 11:51:27 +0800

    同步与异步提交

--- src/main/java/consumer/ConsumerFactory.java
+++ src/main/java/consumer/ConsumerFactory.java
@@ -15,9 +15,10 @@ public class ConsumerFactory {
             return consumer;
         }
         Properties props = new Properties();
-        props.put("bootstrap.servers", "LAPTOP-8UNHPEU7:9092");
+        props.put("bootstrap.servers", "BZD21333-PC.kingsoft.cn:9092");
         props.put("group.id", "logGroup");
         props.put("enable.auto.commit", "false");
+        props.put("auto.offset.reset","earliest");
         props.put("auto.commit.interval.ms", "1000");
         props.put("session.timeout.ms", "30000");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
