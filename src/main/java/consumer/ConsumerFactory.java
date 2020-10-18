80a1c05814c7ae139fbd5bf1f5d2bb241c794437
guanjun <1126964622@qq.com>
2020-10-18 11:23:26 +0800

    消费者

--- src/main/java/consumer/ConsumerFactory.java
+++ src/main/java/consumer/ConsumerFactory.java
@@ -1,6 +1,7 @@
 package consumer;
 
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.util.Objects;
@@ -15,13 +16,13 @@ public class ConsumerFactory {
             return consumer;
         }
         Properties props = new Properties();
-        props.put("bootstrap.servers", "LAPTOP-8UNHPEU7:9092");
-        props.put("group.id", "logGroup");
-        props.put("enable.auto.commit", "false");
-        props.put("auto.commit.interval.ms", "1000");
-        props.put("session.timeout.ms", "30000");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "LAPTOP-8UNHPEU7:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "logGroup");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         consumer = new KafkaConsumer<>(props);
         return consumer;
     }
--- src/main/java/produce/ProducerFactory.java
+++ src/main/java/produce/ProducerFactory.java
@@ -2,6 +2,7 @@ package produce;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,16 +17,16 @@ public class ProducerFactory {
             return producer;
         }
         Properties properties = new Properties();
-        properties.put("partitioner.class", "produce.MyPartitioner");
-        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("bootstrap.servers", "LAPTOP-8UNHPEU7:9092");
-        properties.put("acks", "1");
+        properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, "produce.MyPartitioner");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "LAPTOP-8UNHPEU7:9092");
+        properties.put(ProducerConfig.ACKS_CONFIG, "1");
 
         List<String> interceptors = new ArrayList<>();
         interceptors.add("produce.MyProducerInterceptor");
         interceptors.add("produce.CounterProducerInterceptor");
-        properties.put("interceptor.classes", interceptors);
+        properties.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, interceptors);
         producer = new KafkaProducer<>(properties);
         return producer;
     }
